41cd40bd77bb02e130304766c60b82ecd1ffbfc5
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2017-06-11 16:45:27 -0400

    Profile Page Functionality Sprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProfilePage } from '../pages/profile/profile';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SearchPage } from '../pages/search/search';
+import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -25,6 +26,7 @@ import { HttpModule, JsonpModule } from '@angular/http';
     TabsPage,
     SearchPage,
     LoginPage,
+    SettingsPage,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +43,7 @@ import { HttpModule, JsonpModule } from '@angular/http';
     TabsPage,
     SearchPage,
     LoginPage,
+    SettingsPage,
 
   ],
   providers: [
