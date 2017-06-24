94a8a2e0f34c7edf8176397d2d64cca781f05ba9
bolariinwa <bo.lariin@hotmail.com>
2017-06-24 19:45:40 -0400

    Added horizontal Scroll functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  Favorite: string = "home"
+  items = [
+    {
+      "iconName": "ios-star-outline",
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
-
+    for (let i = 0; i < 30; i++) {
+        this.items.push({
+          "iconName": "ios-star-outline",
+        });
+    }
   }
 
 }
