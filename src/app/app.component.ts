372ec3c6f631bf2d40e152e1430bd23345b87311
bolariinwa <bo.lariin@hotmail.com>
2017-07-02 15:27:57 -0400

    Merge complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,22 @@ export class MyApp {
     constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
         private storage: Storage) {
             storage.clear();
+        /*
+        storage.get('isLoggedIn').then((loginStatus) => {
+            if (loginStatus == true) {
+                this.rootPage = TabsPage;
+            } else {
+                this.rootPage = LoginPage;
+            }
+        });
+        */
+        this.rootPage = TabsPage;
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+
+        });
     }
 }
