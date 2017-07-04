1300be905dbbaff69399f12e0106a15110bea556
ArsalaBangash <arsalabangash@gmail.com>
2017-07-03 20:59:35 -0400

    Home Page cards styled

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,97 +14,92 @@ import 'rxjs/add/operator/map';
 
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 
 export class HomePage {
-  storageService: Storage;
-  EventService: EventService;
-  UserService: UserService;
+	storageService: Storage;
+	EventService: EventService;
+	UserService: UserService;
 
-  currentUser: User;
-  currentUserID: string;
+	currentUser: User;
+	currentUserName: string;
+	currentUserID: string;
 
-  userEventID: String[];
-  // userEvents: Event[];
-  
-  items;
+	userEventID: String[];
+	eventsPage = EventsPage;
 
-  eventsPage = EventsPage;
-  
-  userEvents = [
-    {
-      name: "Event1",
-      city: "Mississauga",
-      country: "Canada",
-      address: "3359 Mississauga Rd, Mississauga, Ontario",
-      start: "July 2nd",
-      end: "July 2nd",
-      picture: "no pic",
-      links: [],
-      type: "meetup",
-      description: "Regular Event"
-    },
-    {
-      name: "Event2",
-      city: "Toronto",
-      country: "Canada",
-      address: "459 Spadina Avenue, Toronto, Ontario",
-      start: "July 3nd",
-      end: "July 3nd",
-      picture: "no pic",
-      links: [],
-      type: "meetup",
-      description: "Regular Event"
-    },
-    {
-      name: "Event3",
-      city: "Mississauga",
-      country: "Canada",
-      address: "3359 Mississauga Rd, Mississauga, Ontario",
-      start: "July 3nd",
-      end: "July 3nd",
-      picture: "no pic",
-      links: [],
-      type: "meetup",
-      description: "Regular Event"
-    }
-  ];
+	userEvents = [
+		{
+			name: "DeerHunt",
+			city: "Mississauga",
+			country: "Canada",
+			address: "3359 Mississauga Rd, Mississauga, Ontario",
+			start: "July 2nd",
+			end: "July 2nd",
+			picture: "no pic",
+			links: [],
+			type: "meetup",
+			description: "Regular Event"
+		},
+		{
+			name: "NodeSchools",
+			city: "Toronto",
+			country: "Canada",
+			address: "459 Spadina Avenue, Toronto, Ontario",
+			start: "July 3nd",
+			end: "July 3nd",
+			picture: "no pic",
+			links: [],
+			type: "meetup",
+			description: "Regular Event"
+		},
+		{
+			name: "Intro to EMACS Workshop",
+			city: "Mississauga",
+			country: "Canada",
+			address: "3359 Mississauga Rd, Mississauga, Ontario",
+			start: "July 3nd",
+			end: "July 3nd",
+			picture: "no pic",
+			links: [],
+			type: "meetup",
+			description: "Regular Event"
+		}
+	];
 
-  constructor(public navCtrl: NavController, private http: Http, private endpoints: Endpoints, private storage: Storage) {
-    
-    this.storageService = storage;
-    this.EventService = new EventService(http, endpoints);
-    this.UserService = new UserService(http, endpoints);
+	constructor(public navCtrl: NavController, private http: Http,
+		private endpoints: Endpoints, private storage: Storage) {
 
-    /*
-    this.storageService.get('currentUser').then(
-      (user) => this.getUserEvents(user._id));
-    */
+		this.storageService = storage;
+		this.EventService = new EventService(http, endpoints);
+		this.UserService = new UserService(http, endpoints);
 
 
-
-  }
-
-
-  /*
-  private getUserEvents(user_id: string) {
-    this.UserService.getUserEvents(user_id)
-      .subscribe((callback: String[]) => this.userEventID = callback,
-                  (error) => console.log(error),
-                  () => this.userEvents = this.userEventID.map(event => this.getEvent(event)));
-  }
-
-  private getEvent(event_id: String): Event {
-    var userEvent: Event;
-    this.EventService.getEvent(event_id)
-      .subscribe((callback: Event) => userEvent = callback);
-    return userEvent;
+		this.storageService.get('currentUser').then((user) => {
+            this.currentUserName = user.name;
+        });
+	}
 
 
-  }
-  */
+	/*
+	private getUserEvents(user_id: string) {
+	  this.UserService.getUserEvents(user_id)
+		.subscribe((callback: String[]) => this.userEventID = callback,
+					(error) => console.log(error),
+					() => this.userEvents = this.userEventID.map(event => this.getEvent(event)));
+	}
+
+	private getEvent(event_id: String): Event {
+	  var userEvent: Event;
+	  this.EventService.getEvent(event_id)
+		.subscribe((callback: Event) => userEvent = callback);
+	  return userEvent;
+
+
+	}
+	*/
 
 
 }
